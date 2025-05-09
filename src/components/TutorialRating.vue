8469b1a3fce73b9f89a57fe6d7070969fcb7a7f3
rael <65247201+penisalado@users.noreply.github.com>
2025-05-09 20:15:48 -0300

    Updated TutorialRating.vue

--- src/components/TutorialRating.vue
+++ src/components/TutorialRating.vue
@@ -16,11 +16,11 @@ const error = ref('')
 const isOpen = ref(false)
 
 const ratingDescriptions = {
-5: 'Muito claro e objetivo',
-4: 'Fácil de entender',
+1: 'Muito claro e objetivo',
+2: 'Fácil de entender',
 3: 'Compreensível',
-2: 'Confuso em partes',
-1: 'Difícil de entender' 
+4: 'Confuso em partes',
+5: 'Difícil de entender'
   }
 
 const fetchRatings = async () => {
